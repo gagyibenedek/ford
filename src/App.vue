7d61c41d1cbf7658d905fd1f93eee98a345c916b
Benedek Gagyi <gagyibenedek@yahoo.com>
2016-11-08 22:09:25 +0100

    First

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <task v-for="(task, index) in tasks"
+      v-bind:finished="task.finished"
+      v-bind:name="task.name"
+      @toggle="update(index, $event)">
+    </task>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Task from './components/Task'
+
+export default {
+  name: 'app',
+  components: {
+    Task
+  },
+  data () {
+    return {
+      tasks: [
+        {
+          finished: false,
+          name: 'task1'
+        },
+        {
+          finished: false,
+          name: 'task2'
+        },
+        {
+          finished: true,
+          name: 'task3'
+        }
+      ]
+    }
+  },
+  methods: {
+    update (index, event) {
+      Vue.set(this.tasks[index], 'finished', event)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="task">
+    <input type="checkbox" :checked="finished" v-on:click="toggle" />
+    <label class="task-name" :class="{finished:finished}">{{name}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  props: {
+    'finished': {
+      type: Boolean,
+      required: true
+    },
+    'name': {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    toggle () {
+      this.$emit('toggle', !this.finished)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.task-name {
+  vertical-align: top;
+}
+
+.task-name.finished {
+  text-decoration: line-through;
+}
+
+</style>
